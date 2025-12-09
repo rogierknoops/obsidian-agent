a7304572d384ad93d3cdd0fa498d9815ae1019d1
Rogier Knoops <rogier.knoops@gmail.com>
2025-12-09 14:19:12 +0100

    Default log to ./answer.md in run-agent.sh

--- run-agent.sh
+++ run-agent.sh
@@ -8,5 +8,19 @@ if [[ -d ".venv" ]]; then
   source ".venv/bin/activate"
 fi
 
-python3 main.py chat "$@"
+# Default log file unless user already provided --log-md
+DEFAULT_LOG="./answer.md"
+USE_LOG=true
+for arg in "$@"; do
+  if [[ "$arg" == --log-md* ]]; then
+    USE_LOG=false
+    break
+  fi
+done
+
+if $USE_LOG; then
+  python3 main.py chat --log-md "$DEFAULT_LOG" "$@"
+else
+  python3 main.py chat "$@"
+fi
 
